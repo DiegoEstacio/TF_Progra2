7d16bec76dbb0724ffa0ea12c899a10b9b731461
Bruce Fleck Ojeda <u20211e803@upc.edu.pe>
2022-02-14 10:30:27 -0500

    Agregue formulario

---
+++ TF_Progra2/formulario.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace TFProgra2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for formulario
+	/// </summary>
+	public ref class formulario : public System::Windows::Forms::Form
+	{
+	public:
+		formulario(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~formulario()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// formulario
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(14, 29);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(2105, 1079);
+			this->Name = L"formulario";
+			this->Text = L"formulario";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
