9e2e21cc3de8801af50b59b6da2f309d4ee10e38
DiegoEstacio <99835546+DiegoEstacio@users.noreply.github.com>
2022-02-16 14:32:47 -0500

    Update CComponentes.h

--- TF_Progra2/CComponentes.h
+++ TF_Progra2/CComponentes.h
@@ -1,36 +1,46 @@
 
+using namespace System::Drawing;
 
+enum Direcciones { Ninguna, Abajo, Arriba, Izquierda, Derecha };
 class CComponentes {
+protected:
 	int x, y, dx, dy;
+	int ancho, alto;
+	int indicex, indicey;
+	Direcciones ultimaTecla;
+	Direcciones direccion;
 
 public:
 	CComponentes() {};
-
-	void setX(int x) {
-		this->x = x;
+	CComponentes(int _x, int _y) {
+		x = _x;
+		y = _y;
+		dx = 0;
+		dy = 0;
+		ultimaTecla = Abajo;
+		direccion = Ninguna;
+		indicex = 0;
+		indicey = 0;
 	}
-	
-	void setY(int y) {
-		this->y = y;
+	~CComponentes() {};
+	void setX(int x) {this->x = x;}
+	void setY(int y) {this->y = y;}
+
+	int getX() {return this->x;}
+	int getY() {return this->y;}
+
+	void setDirecciones(Direcciones _direccion) { x = _direccion; }
+	int getDireccion() { return direccion; }
+
+	void pintar(BufferedGraphics^ bf, Bitmap^mapa) {
+		Rectangle areaUsar = Rectangle(ancho * indicex, alto * indicey, ancho, alto);
+		Rectangle areaDibujar = Rectangle(ancho * indicex, alto * indicey, ancho * 1.2, alto * 1.2);
+		bf->Graphics->DrawImage(mapa, areaDibujar, areaUsar, GraphicsUnit::Pixel);
+		x += dx;
+		y += dy;
 	}
 
-	int getX() {
-		return this->x;
-	}
-
-	int getY() {
-		return this->y;
-	}
-
-	void borrar() {
-
-	}
-
-	void pintar() {
-
-	}
-
-	void cambioPosicion() {
-
+	void cambioPosicion(BufferedGraphics^ bf, Bitmap^ mapa) {
+		
 	}
 };
